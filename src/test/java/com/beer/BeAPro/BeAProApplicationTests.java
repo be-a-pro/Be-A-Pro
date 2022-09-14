f6dd9f88ae2adbcd0fdd045f06f9fb3e067658e3
u-nij <u.nij0509@gmail.com>
2022-09-15 01:59:50 +0900

    Spring Boot + React.js 개발환경 세팅

---
+++ src/test/java/com/beer/BeAPro/BeAProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beer.BeAPro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeAProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
