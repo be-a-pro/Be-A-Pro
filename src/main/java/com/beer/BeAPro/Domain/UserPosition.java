9c11a3841e5733c13bed8c54315428085f10d819
u-nij <u.nij0509@gmail.com>
2022-09-16 14:47:31 +0900

    Feat: 엔티티 생성 (#5)
    - 사용자 포지션 엔티티 생성: UserPosition

--- src/main/java/com/beer/BeAPro/Domain/UserPosition.java
+++ src/main/java/com/beer/BeAPro/Domain/UserPosition.java
@@ -2,10 +2,7 @@ package com.beer.BeAPro.Domain;
 
 import lombok.Getter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
@@ -15,4 +12,15 @@ public class UserPosition {
     @Column(name = "user_position_id")
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user; // 사용자
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "position_id")
+    @Column(unique = true) // FK
+    private Position position; // 사용자의 포지션
+
+    @Enumerated(EnumType.STRING)
+    private Career career; // 해당 포지션 경력
 }
