4acd01e1c74f7060e14ae1b9d4966feaa09b5a7f
u-nij <u.nij0509@gmail.com>
2022-10-06 16:49:07 +0900

    Feat:  Spring Security 및 JWT 관련 인터페이스 상속 및 구현(#13)
    - SecurityConfig: Spring Security 설정
    - JwtAccessDeniedHandler: 액세스 불가능한 권한의 유저가 요청했을 경우 동작
    - JwtAuthenticationEntryPoint: 인증이 되지 않은 유저가 요청을 보냈을 때 동작
    - JwtAuthenticationFilter: 토큰 추출 후 정상 토큰인지 검사 후, Authentication 정보를 SecurityContextHolder에 저장
    - JwtTokenProvider: 토큰 생성, 검증, 토큰으로부터 정보 추출
    - UserDetailsImpl, UserDetailsServiceImpl: User.email과 User.password를 principal과 credential로 사용해 사용자 검증

---
+++ src/main/java/com/beer/BeAPro/Security/JwtAuthenticationFilter.java
@@ -0,0 +1,61 @@
+package com.beer.BeAPro.Security;
+
+import io.jsonwebtoken.IncorrectClaimException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationFilter(JwtTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        // JWT Token 추출
+        String token = resolveToken(request);
+
+        try { // 정상 토큰인지 검사
+            if (token != null && jwtTokenProvider.validateToken(token)) {
+                Authentication authentication = jwtTokenProvider.getAuthentication(token);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                log.debug("Save authentication in SecurityContextHolder.");
+            }
+        } catch (IncorrectClaimException e) { // 잘못된 토큰일 경우
+            SecurityContextHolder.clearContext();
+            log.debug("Invalid JWT token.");
+            response.sendError(403);
+        } catch (UsernameNotFoundException e) { // 회원을 찾을 수 없을 경우
+            SecurityContextHolder.clearContext();
+            log.debug("Can't find user.");
+            response.sendError(403);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    // HTTP Request 헤더로부터 토큰 추출
+    public String resolveToken(HttpServletRequest httpServletRequest) {
+        String bearerToken = httpServletRequest.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/beer/BeAPro/Security/SecurityConfig.java
@@ -0,0 +1,80 @@
+package com.beer.BeAPro.Security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity // Spring Security 설정 클래스
+@EnableGlobalMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+
+    @Bean
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        // ACL(Access Control List, 접근 제어 목록)의 예외 URL 설정
+        return (web)
+                -> web
+                .ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // 인터셉터로 요청을 안전하게 보호하는 방법 설정
+        http
+                // jwt 토큰 사용을 위한 설정
+                .csrf().disable()
+                .cors().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                // 예외 처리
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint) //customEntryPoint
+                .accessDeniedHandler(jwtAccessDeniedHandler) // cutomAccessDeniedHandler
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/api/mypage/**").authenticated() // 마이페이지 인증 필요
+                .antMatchers("/api/admin/**").hasRole("ADMIN") // 관리자 페이지
+                .anyRequest().permitAll()
+
+                // X-Frame-Options
+                .and()
+                .headers()
+                .frameOptions().sameOrigin();
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+}
