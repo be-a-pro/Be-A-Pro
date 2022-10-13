c55f722809317f7fb8294ed654efc2de8e356798
u-nij <u.nij0509@gmail.com>
2022-10-13 19:16:03 +0900

    Feat: @RestControllerAdvice를 이용한 전역 예외 처리(#17)
    - ErrorCode: 예외 케이스 관리
    - ErrorResponse: 사용자에게 보여질 에러 응답 형식 지정
    - RestApiException: RuntimeException을 상속받은 에러 클래스
    - GlobalExceptionHandler: ResponseEntityExceptionHandler을 상속받아 각 에러 클래스에 대한 예외 처리

---
+++ src/main/java/com/beer/BeAPro/Exception/ErrorResponse.java
@@ -0,0 +1,19 @@
+package com.beer.BeAPro.Exception;
+
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class ErrorResponse {
+    private final LocalDateTime timestamp = LocalDateTime.now();
+    private final int statusCode;
+    private final String error;
+    private final String message;
+
+    public ErrorResponse(ErrorCode errorCode) {
+        this.statusCode = errorCode.getHttpStatus().value();
+        this.error = errorCode.getHttpStatus().name();
+        this.message = errorCode.getMessage();
+    }
+}
