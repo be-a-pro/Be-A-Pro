b0b18f126825894f62ef2959bf40db9810eceafe
u-nij <u.nij0509@gmail.com>
2022-10-29 22:43:23 +0900

    Feat: 추가 정보 입력 API 생성(#33)
    - 추가 정보 입력: 전화번호 공개 여부, 포지션, 관심 키워드, 사용 툴, 포트폴리오 공개 여부/파일/링크
    - Validation: 전화번호 공개 여부, 포트폴리오 공개 여부는 필수. 포트폴리오 링크는 최대 3개.
    - 약관 동의 여부가 저장되지 않았을 경우, 상태 코드 401 반환
    - 포트폴리오 파일 AWS S3에 업로드 및 정보 저장
      - 확장자 검사 및 용량 검사

--- src/main/java/com/beer/BeAPro/Service/FileUploadService.java
+++ src/main/java/com/beer/BeAPro/Service/FileUploadService.java
@@ -3,7 +3,6 @@ package com.beer.BeAPro.Service;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.beer.BeAPro.Domain.PortfolioFile;
 import com.beer.BeAPro.Domain.ProfileImage;
-import com.beer.BeAPro.Domain.Project;
 import com.beer.BeAPro.Domain.ProjectImage;
 import com.beer.BeAPro.Dto.FileUploadDto;
 import com.beer.BeAPro.Exception.ErrorCode;
@@ -12,15 +11,18 @@ import com.beer.BeAPro.Repository.PortfolioFileRepository;
 import com.beer.BeAPro.Repository.ProfileImageRepository;
 import com.beer.BeAPro.Repository.ProjectImageRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.UUID;
 
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class FileUploadService {
@@ -30,21 +32,44 @@ public class FileUploadService {
     private final ProjectImageRepository projectImageRepository;
     private final PortfolioFileRepository portfolioFileRepository;
 
+    private final String[] fileExtension = {".jpg", ".png", ".pdf", ".ppt", ".pptx", ".hwp", ".hwpx"};
+    private final String[] imageExtension = {".png", ".jpg", ".jpeg", ".gif"};
+
 
     // 파일의 확장자 추출
     public String getFileExtension(String originalName) {
         try {
             return originalName.substring(originalName.lastIndexOf("."));
         } catch (StringIndexOutOfBoundsException e) {
-            throw new IllegalArgumentException("잘못된 형식의 파일입니다.: " + originalName);
+            throw new RestApiException(ErrorCode.INVALID_FILE_NAME); // 잘못된 형식의 파일명
+        }
+    }
+
+    // 알맞은 확장자가 아닐 경우 false 반환
+    public boolean checkExtension(String extension, String type) {
+        if (type.equals("file")) {
+            return Arrays.asList(fileExtension).contains(extension);
+        } else if (type.equals("image")) {
+            return Arrays.asList(imageExtension).contains(extension);
+        } else {
+            return false;
         }
     }
 
     // AWS S3에 파일 업로드
     @Transactional
-    public FileUploadDto uploadFile(MultipartFile file, long sizeLimit) {
+    public FileUploadDto uploadFile(MultipartFile file, long sizeLimit, String type) {
         String originalName = file.getOriginalFilename();
-        String modifiedName =  UUID.randomUUID().toString().concat(getFileExtension(originalName));
+        if (originalName == null) {
+            originalName = type;
+        }
+
+        String extension = getFileExtension(originalName);
+        if (!checkExtension(extension, type)) {
+            throw new RestApiException(ErrorCode.INVALID_EXTENSION);
+        }
+
+        String modifiedName =  UUID.randomUUID().toString().concat(extension);
         long size = file.getSize();
         if (size >= sizeLimit) {
             throw new RestApiException(ErrorCode.FILE_TOO_LARGE);
@@ -56,7 +81,8 @@ public class FileUploadService {
         try (InputStream inputStream = file.getInputStream()) {
             awsS3UploadService.uploadFile(inputStream, objectMetadata, modifiedName);
         } catch (IOException e) {
-            throw new IllegalArgumentException("파일 변환 중 에러가 발생했습니다.");
+            log.error("An error occurred during file conversion.");
+            throw new RuntimeException("Error not defined.");
         }
 
         return FileUploadDto.builder()
