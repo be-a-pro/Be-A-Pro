140b8743917555d3f9e7ad422c3cd70898990e84
u-nij <u.nij0509@gmail.com>
2022-12-15 16:39:01 +0900

    Feat: 사용자 회원가입 후 추가 정보 입력 로직 수정(#82)
    사용자 회원 가입 후 추가 정보 입력시, 기존 정보가 있을 경우 삭제 후 재생성

--- src/main/java/com/beer/BeAPro/Repository/UserToolRepository.java
+++ src/main/java/com/beer/BeAPro/Repository/UserToolRepository.java
@@ -1,7 +1,11 @@
 package com.beer.BeAPro.Repository;
 
+import com.beer.BeAPro.Domain.User;
 import com.beer.BeAPro.Domain.UserTool;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserToolRepository extends JpaRepository<UserTool, Long> {
+    List<UserTool> findAllByUser(User user);
 }
